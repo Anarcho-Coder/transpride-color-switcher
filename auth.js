dd8d30e04daeca3b657370a0e6e9e36e69dc32b9
adi-dev-1337 <adi.dev.1337@gmail.com>
2022-04-09 22:09:24 +0100

    testing riskiness of auth commit

--- auth.js
+++ auth.js
@@ -3,3 +3,8 @@ const TMI_OAUTH = "AUTH_KEY_HERE";
 
 exports.BOT_NAME = BOT_NAME;
 exports.TMI_OAUTH = TMI_OAUTH;
+
+//-----------------------Developer warning------------------------
+//
+// Please remember to discard changes to this file before a commit.
+// Commits containing a change to this file will be rolled back.
